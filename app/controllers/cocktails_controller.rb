e9adb7dc41fa42cd0450506e56c6305a81038ee6
Edgar Marques <edgar.eu1@hotmail.com>
2020-08-07 20:03:31 +0100

    Change sorting specs to display Cocktails without ingredients unless filtered

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -6,11 +6,12 @@ class CocktailsController < ApplicationController
     @ingredients = Ingredient.all.sort_by(&:name)
 
     if params[:query].present?
-      @cocktails_sorted = Cocktail.search_by_name(params[:query]).paginate(page: params[:page], per_page: 12)
+      @cocktails_sorted = Cocktail.search_by_name(params[:query])
     else
-      @cocktails = Cocktail.all
-      @cocktails_sorted = cocktails_sorter(@cocktails).paginate(page: params[:page], per_page: 12)
+      @cocktails_sorted = cocktails_sorter(Cocktail.all)
     end
+
+    @cocktails_sorted = @cocktails_sorted.paginate(page: params[:page], per_page: 12)
   end
 
   def show
@@ -39,15 +40,15 @@ class CocktailsController < ApplicationController
   private
 
   def cocktails_sorter(cocktails)
-    # Only cocktails with ingredients will be displayed
-    relevant_cocktails = cocktails.reject { |cocktail| cocktail.ingredients.empty? }
-
     # Sorting by popularity takes into account the cocktail's number of reviews
     if params[:sort_by] == "popularity"
-      return relevant_cocktails.sort_by { |cocktail| - cocktail.reviews.size }
+      return cocktails.sort_by { |cocktail| - cocktail.reviews.size }
 
     # Sorting by filter takes into account the ingredient list selected by the user
     elsif params[:sort_by] == "Filter" && !params[:ingredients].nil?
+      # Only cocktails with ingredients should be displayed
+      relevant_cocktails = cocktails.reject { |cocktail| cocktail.ingredients.empty? }
+
       # The cocktails will first be sorted based on the number of ingredients in common with the
       # user's list and then sorted by ranking
       relevant_cocktails.map! do |cocktail|
@@ -73,7 +74,7 @@ class CocktailsController < ApplicationController
 
     # By default cocktails are sorted by ranking; If the user chooses to filter by ingredients but
     # does not select any, the sorting will also be done by ranking
-    return relevant_cocktails.sort_by { |cocktail| - cocktail.rating_points }
+    return cocktails.sort_by { |cocktail| - cocktail.rating_points }
   end
 
   def cocktail_params
