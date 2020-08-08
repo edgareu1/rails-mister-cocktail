4a64eadfe80c2a20ab5492e81a39dcc5c3dafaba
Edgar Marques <edgar.eu1@hotmail.com>
2020-08-08 12:36:12 +0100

    Add Instructions column to the Cocktails Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_112658) do
+ActiveRecord::Schema.define(version: 2020_08_08_113225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_08_08_112658) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "alcoholic"
+    t.string "instructions", default: [], array: true
   end
 
   create_table "doses", force: :cascade do |t|
