36830de8a4a0ae684d6c2cf0055241f19ff8920f
Edgar Marques <edgar.eu1@hotmail.com>
2020-08-10 20:01:45 +0100

    Add the option to edit an existing Dose

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   root to: "cocktails#index"
 
   resources :cocktails do
     resources :doses, only: [:create]
     resources :reviews, only: [:create]
   end
-  resources :doses, only: [:destroy]
 
+  resources :doses, only: [:destroy, :update]
 end
