93ce36858c1346ccb483717f43b421c34dd81221
Edgar Marques <edgar.eu1@hotmail.com>
2020-08-11 03:30:15 +0100

    Change the website color gradients

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,8 +10,8 @@ require("channels")
 
 import 'bootstrap';
 
-import { smothScrolling } from '../custom/read-more';
 import { readMore } from '../custom/read-more';
+import { smothScrolling } from '../custom/smoth-scrolling';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
