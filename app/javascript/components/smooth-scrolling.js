c8132f8fcbec172878ab4418508b169c97b80cc9
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-12 00:39:36 +0100

    Make it so that the anchor to the cocktails list doesn't refresh the page

--- app/javascript/components/smooth-scrolling.js
+++ app/javascript/components/smooth-scrolling.js
@@ -1,21 +1,12 @@
-// Function that adds smooth scrolling to the same page links
+// jQuery Function that adds smooth scrolling to the same page links
 function smoothScrolling() {
-  $("#scroller").on('click', function(event) {
-    // Makes sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevents default anchor click behavior
-      event.preventDefault();
+  $('#scroller').on('click', function(event) {
+    event.preventDefault(); // Prevents default anchor click behavior
 
-      var hash = this.hash;
-
-      // Uses jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 600, function(){
-        window.location.hash = hash;
-      });
-    }
+    // Performs the scrolling animation during 800ms
+    $("html, body").animate( {
+      scrollTop: $('#container-cards').offset().top
+    }, 800);
   });
 };
 
