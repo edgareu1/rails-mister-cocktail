fe8f98881a287e5415cf7c89ab2f82b0a548733e
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-12 18:31:47 +0100

    Ajaxify the Doses controller related actions

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,41 +1,54 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:create, :update, :destroy]
+
   def create
-    @cocktail = Cocktail.find(params[:cocktail_id])
-
-    unless @cocktail.doses.find_by(ingredient_id: params["dose"]["ingredient_id"]).nil?
-      update
-
-    else
+    # If a dose with that ingredient does not exist, then create a new one
+    # Otherwise update the already existing dose with the same ingredient
+    if @cocktail.doses.find_by(ingredient_id: params["dose"]["ingredient_id"]).nil?
       @dose = Dose.new(dose_params)
       @dose.cocktail = @cocktail
 
       if @dose.save
-        redirect_to cocktail_path(@cocktail, anchor: "ingredients-list")
+        # For the AJAX requests
+        respond_to do |format|
+          format.html
+          format.js
+        end
+
+      # In case there's an error saving the new dose
       else
         @review = Review.new
         render 'cocktails/show'
       end
+
+    else
+      update
     end
   end
 
   def update
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = @cocktail.doses.find_by(ingredient_id: params["dose"]["ingredient_id"])
     @dose.update(dose_params)
-
-    redirect_to cocktail_path(@cocktail, anchor: "ingredients-list")
   end
 
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
 
-    redirect_to cocktail_path(@dose.cocktail, anchor: "ingredients-list")
+    # For the AJAX requests
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   private
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 end
