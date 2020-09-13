a45382b9cd31219ab0689007856a8fbfef2d3e18
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-13 14:17:32 +0100

    Make the Modal actions of the Reviews controller more user friendly

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,15 +4,8 @@ class ReviewsController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
     @review.cocktail = @cocktail
 
-    if @review.save
-      # For the AJAX requests
-      respond_to do |format|
-        format.html
-        format.js
-      end
-
     # In case there's an error saving the new review
-    else
+    unless @review.save
       @dose = Dose.new
       render 'cocktails/show'
     end
