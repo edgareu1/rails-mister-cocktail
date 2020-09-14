85b142bb1730a66c95534f9273c4c3bfee6871df
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-14 15:07:08 +0100

    Get the Cocktails that include the search param

--- app/javascript/components/auto-complete-cocktail.js
+++ app/javascript/components/auto-complete-cocktail.js
@@ -1,5 +1,17 @@
 function autoCompleteCocktail() {
-  const cocktails_names = gon.cocktails_names.split(' -/- ');     // Array of Cocktails to search into
+  const searchField = document.getElementById('search-input');    // Search field element
+
+  searchField.addEventListener('input', (event) => {
+    const param = event.target.value.trim();                      // Search param striped of trailing whitespaces
+    const coktaislNames = gon.cocktails_names.split(' -/- ');     // Array of Cocktails to search into
+
+    for (let i = 0; i < coktaislNames.length; i++) {
+      // Iterate over the array of Cocktails and get the elements that include the search param
+      if (coktaislNames[i].toUpperCase().includes(param.toUpperCase())) {
+        console.log(coktaislNames[i]);
+      }
+    }
+  });
 }
 
 export { autoCompleteCocktail };
