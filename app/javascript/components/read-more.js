1806936c7593ae9ae85ae92751decd7ac93d216b
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-15 12:58:24 +0100

    Change the JS readMore funtion to be based on line height

--- app/javascript/components/read-more.js
+++ app/javascript/components/read-more.js
@@ -1,52 +1,27 @@
 // Function that will hide the text that exceds a certain length, while giving the user the option
 // to view it (view more); if this option is selected, the user may also choose 'read less'
 function readMore() {
-  var showChar = 140;
-  var moretext = '&nbsp;Read more';
-  var lesstext = '&nbsp;Read less';
+  var showHeight = 42;
+  var moretext = '<span class="elipse">...</span>&nbsp;Read more';
+  var lesstext = 'Read less';
 
   var reviewsList = document.getElementsByClassName('read-more');
 
   for (let i = 0; i < reviewsList.length; i++) {
     var element = reviewsList[i];
-    var content = element.innerHTML;
 
-    if(content.length > showChar) {
-      var c = content.substr(0, showChar);
-      var h = content.substr(showChar, content.length - showChar);
-      var html = c
-        + '<span class="elipse-text">...</span>'
-        + '<span class="morecontent">'
-        +   '<span class="hidden-content">'
-        +     h
-        +   '</span>'
-        +   '<a href="" class="morelink">'
-        +     moretext
-        +   '</a>'
-        + '</span>';
+    if(element.clientHeight > showHeight) {
+      element.style.height = `${showHeight}px`;
 
-      element.innerHTML = html;
+      let elementParent = element.parentElement;
+      elementParent.style.position = 'relative';
+      elementParent.style.marginBottom = '32px';
 
-      element.lastChild.lastChild.addEventListener('click', (event) => {
-        event.preventDefault();
+      let link = document.createElement("a");
+      link.innerHTML = moretext;
+      link.classList.add('morelink');
 
-        var target = event.target;
-
-        if (target.classList.contains('less')) {
-          target.classList.remove('less');
-          target.innerHTML = moretext;
-
-          target.previousSibling.style.display = 'none';
-          target.parentElement.previousSibling.style.display = 'contents';
-
-        } else {
-          target.classList.add('less');
-          target.innerHTML = lesstext;
-
-          target.previousSibling.style.display = 'contents';
-          target.parentElement.previousSibling.style.display = 'none';
-        }
-      });
+      element.parentElement.appendChild(link);
     }
   }
 };
