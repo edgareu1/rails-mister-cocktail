54f10cd7197d090f82fd66a111a49109fb32f7b4
Edgar Marques <edgar.eu1@hotmail.com>
2020-09-15 13:28:48 +0100

    Refactor and comment the JS readMore function

--- app/javascript/components/read-more.js
+++ app/javascript/components/read-more.js
@@ -1,44 +1,54 @@
-// Function that will hide the text that exceds a certain length, while giving the user the option
-// to view it (view more); if this option is selected, the user may also choose 'read less'
+// Function that will hide the text that exceds a certain height, while giving the user the option
+// to view it ('read more'); if this option is selected, the user can also choose to 'read less'
 function readMore() {
-  var showHeight = 42;
+  var showHeight = 42;  // Height limit that the review content paragraph can take initially
+
+  // Text that will appear on each link
   var moretext = '<span class="elipse">...</span>&nbsp;Read more';
   var lesstext = 'Read less';
 
-  var reviewsList = document.getElementsByClassName('read-more');
+  var reviewsList = document.getElementsByClassName('read-more');   // List of reviews content
 
+  // Iterate over the list
   for (let i = 0; i < reviewsList.length; i++) {
     var element = reviewsList[i];
 
+    // If the element shows more content than permited (his height surpasses the limit), then...
     if(element.clientHeight > showHeight) {
-      element.style.height = `${showHeight}px`;
+      element.style.height = `${showHeight}px`;   // Change it's height to the height limit
 
+      // Change the parent to accommodate the changes
       let elementParent = element.parentElement;
       elementParent.style.position = 'relative';
       elementParent.style.marginBottom = '32px';
 
-      let link = document.createElement("a");
-      link.innerHTML = moretext;
-      link.classList.add('morelink');
+      // Create the link to change the content displayed
+      let readLink = document.createElement("a");
+      readLink.innerHTML = moretext;
+      readLink.classList.add('morelink');
 
-      element.parentElement.appendChild(link);
+      elementParent.appendChild(readLink);  // Append link to the document
 
-      link.addEventListener('click', (event) => {
-        event.preventDefault();
+      // Add the toggle behaviour to the link
+      readLink.addEventListener('click', (event) => {
+        event.preventDefault();   // Prevent the default behaviour of the anchor
 
         var target = event.target;
 
+        // If the 'read less' link is clicked, then...
         if (target.classList.contains('less')) {
           target.classList.remove('less');
-          target.innerHTML = moretext;
+          target.innerHTML = moretext;  // Change the link text
 
+          // Hide the review content that exceds the height limit
           target.previousElementSibling.style.height = `${showHeight}px`;
 
+        // If the 'read more' link is clicked, then...
         } else {
           target.classList.add('less');
-          target.innerHTML = lesstext;
+          target.innerHTML = lesstext;  // Change the link text
 
-          target.previousElementSibling.style.height = 'fit-content';
+          target.previousElementSibling.style.height = 'fit-content';   // Show the hidden review content
         }
       });
     }
